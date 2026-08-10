8e76a22ffe3cb37bb2f868387208bb51178d31bc
GANESAN G <ganesan184510@gmail.com>
2026-08-10 21:59:52 +0530

    Time: 1271 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,8 @@
+SELECT e1.employee_id,e1.name,COUNT(e2.reports_to) AS reports_count,
+ROUND(AVG(e2.age)) AS average_age
+FROM Employees e1
+LEFT JOIN Employees e2
+    ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id
+HAVING reports_count > 0
+ORDER BY e1.employee_id ASC;
