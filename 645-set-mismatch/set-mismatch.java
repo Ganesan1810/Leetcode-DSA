239f4f9d7226ebba5847ddc504b79b1bafe21506
GANESAN G <ganesan184510@gmail.com>
2026-08-18 22:31:05 +0530

    Time: 2 ms (94.56%) | Memory: 47.9 MB (24.46%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n= nums.length;
+        int[] count=new int[n+1];
+        int duplicate=-1;
+        int missing=-1;
+        for(int freq:nums)
+            count[freq]++;
+        for(int i=1;i<=n;i++)
+        {
+            if(count[i]==2) 
+                duplicate=i;
+            else if(count[i]==0)
+                missing=i;
+        }
+        return new int[]{duplicate,missing};
+    }
+}
