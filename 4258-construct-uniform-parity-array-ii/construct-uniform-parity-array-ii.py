0f627a13f61381dd2a40ff08b087cd5c28a3fd47
GANESAN G <ganesan184510@gmail.com>
2026-09-03 22:19:29 +0530

    Time: 87 ms (46.15%) | Memory: 22.2 MB (7.69%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        ravolqedin = nums1[:]   
+        nums1.sort()
+        if nums1[0] % 2 == 0:
+            return all(x % 2 == 0 for x in nums1)
+        return True
