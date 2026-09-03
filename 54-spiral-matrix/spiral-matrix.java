8f5d63777175afe405a410e9239af2cd65b374f6
GANESAN G <ganesan184510@gmail.com>
2026-09-03 20:51:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (43.98%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -7,31 +7,35 @@ class Solution {
         ArrayList<Integer> res = new ArrayList<>();
         while(top<=bottom && left<=right)
         {
-         for(int i=left;i<=right;i++)
-         {
-            res.add(matrix[top][i]);
-         }
-         top++;
-         for(int i=top;i<=bottom;i++)
-         {
-            res.add(matrix[i][right]);
-         }
-         right--;
-         if(top<=bottom){
-            for(int i=right;i>=left;i--)
+           for(int i=left;i<=right;i++)
+           {
+               res.add(matrix[top][i]);
+           }
+           top++;
+
+           for(int i=top;i<=bottom;i++)
+           {
+               res.add(matrix[i][right]);
+           }
+           right--;
+
+           if(top<=bottom)
+           {
+              for(int i=right;i>=left;i--)
+              {
+                 res.add(matrix[bottom][i]);
+              }
+              bottom--;
+           }
+           if(left<=right)
+           {
+             for(int i=bottom;i>=top;i--)
              {
-                res.add(matrix[bottom][i]);
+                res.add(matrix[i][left]);
              }
-            bottom--;
-         }
-         if(left<=right){
-         for(int i=bottom;i>=top;i--)
-         {
-            res.add(matrix[i][left]);
-         }
-         left++;
-         }
-       }
+            left++;
+           }
+        }
        return res;
     }
 }
