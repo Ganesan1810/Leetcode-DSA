926e7ad9c9c84f6558158e3d1f34ecf4ad28639c
GANESAN G <ganesan184510@gmail.com>
2026-09-03 22:20:19 +0530

    Time: 93 ms (46.15%) | Memory: 21.8 MB (61.54%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def uniformArray(self, nums1):
-        a= nums1[:]   
         nums1.sort()
         if nums1[0] % 2 == 0:
             return all(x % 2 == 0 for x in nums1)
