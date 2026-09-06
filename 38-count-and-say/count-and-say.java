e6b941b112159759ab04bb7fed9d300143fde6f1
GANESAN G <ganesan184510@gmail.com>
2026-09-06 14:23:56 +0530

    Time: 2 ms (99.69%) | Memory: 42.8 MB (84.19%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,6 +1,7 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1) return "1";
+        if(n==1) 
+            return "1";
 
         String prev=countAndSay(n-1);
         StringBuilder res = new StringBuilder();
