dd2cd0e3b10c6d021a3c6ad6b4bed7328e532b25
GANESAN G <ganesan184510@gmail.com>
2026-09-14 23:03:05 +0530

    Time: 3 ms (94.23%) | Memory: 44 MB (76.43%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -19,8 +19,8 @@ class Solution {
             return;
         list.add(new int[] {c,r,root.val});
 
-        dfs(root.left,r+1,c-1,list);
-        dfs(root.right,r+1,c+1,list);
+        dfs(root.left,r+1,c-1,list); // move left node
+        dfs(root.right,r+1,c+1,list); // move right node 
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         
@@ -28,20 +28,20 @@ class Solution {
         dfs(root,0,0,nodes);
 
         nodes.sort((a,b)->{
-            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]); 
             if(a[1]!=b[1]) return Integer.compare(a[1],b[1]);
             return Integer.compare(a[2],b[2]);
         });
 
         ArrayList<List<Integer>> ans = new ArrayList<>();
         int col = Integer.MIN_VALUE;
-        
+        // column change ana new Arraylist create pannanum
         for(int[] n:nodes){
             if(n[0]!=col){
                 ans.add(new ArrayList<>());
                 col=n[0];
             }
-            ans.get(ans.size()-1).add(n[2]);
+            ans.get(ans.size()-1).add(n[2]); // 
 
         }
         return ans;
