1d8e69fa62171daa2945e998f0acb6507107150a
GANESAN G <ganesan184510@gmail.com>
2026-09-15 09:52:41 +0530

    Time: 1 ms (99.97%) | Memory: 42.7 MB (45.35%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+         int result=0;
+        while(x!=0)
+        {
+        int num=x%10;
+        if(result>Integer.MAX_VALUE / 10|| result< Integer.MIN_VALUE /10){
+            return 0;
+        }
+       
+        result=result*10+num;
+        x=x/10;
+        } return(result);
+    }
+       
+    
+}
